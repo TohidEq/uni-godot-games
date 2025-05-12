20fb02934b90df5fcc51c20d66a1f571c6be1ccf
Tohid Eghdami <TohidEq@gmail.com>
2025-05-12 22:27:04 +0330

    Gun and bullet

---
+++ simple-platformer/scripts/bullet.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+const SPEED : int = 300;
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position += transform.x * SPEED * delta;
+	
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free();
---
+++ simple-platformer/scripts/gun.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var marker: Marker2D = $Marker2D
+
+const BULLET = preload("res://scenes/bullet.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	look_at(get_global_mouse_position());
+	# Fix left side looking sprite
+	rotation_degrees = wrap(rotation_degrees,0,360);
+	if rotation_degrees > 90 and rotation_degrees < 270:
+		if scale.y > 0:
+			scale.y *= -1;
+	else:
+		if scale.y < 0:
+			scale.y *= -1;
+	
+	if Input.is_action_just_pressed("fire"):
+		var bullet_instance = BULLET.instantiate();
+		get_tree().root.add_child(bullet_instance);
+		bullet_instance.global_position = marker.global_position;
+		bullet_instance.rotation = rotation;
