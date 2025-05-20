3e546f3ca52598207ec96df3c4a1182afae5b1d0
Tohid Eghdami <TohidEq@gmail.com>
2025-05-20 13:19:51 +0330

    Gun Follows Mobs

--- deadlydarlings/gun/gun.gd
+++ deadlydarlings/gun/gun.gd
@@ -1,3 +1,20 @@
 extends Area2D
 
+@onready var spr_gun: Sprite2D = $WeaponPivot/Sprite2D
 
+
+func _physics_process(delta: float) -> void:
+  var enemyes_in_range = get_overlapping_bodies()
+
+  if enemyes_in_range.size() > 0:
+    # var target_enemy = enemyes_in_range[0]
+    var target_enemy = enemyes_in_range.front()
+    look_at(target_enemy.global_position)
+    
+    
+    var angle_deg = float(int(rad_to_deg(rotation)) % 360);
+    print(angle_deg)
+    if angle_deg > 90 and angle_deg < 270:
+      spr_gun.scale.y = -1
+    else:
+      spr_gun.scale.y = 1
